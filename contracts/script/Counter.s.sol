b5ee6b147be388dc008e2f8ae178376ec3f374f3
mathrews <mateusps4gg@gmail.com>
2024-06-08 10:22:08 -0300

    solving some errors

--- contracts/script/Counter.s.sol
+++ contracts/script/Counter.s.sol
@@ -2,11 +2,18 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-
+import {Counter} from "../src/Counter.sol";
 contract CounterScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
+        uint256 deployerPrivateKey = vm.envUint("$PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        Counter counter = new Counter();
+        console.log("Counter address: ", address(counter));
+        counter.setNumber(69420);
+        console.log("Counter number: ", counter.number());
+        vm.stopBroadcast();
     }
 }
